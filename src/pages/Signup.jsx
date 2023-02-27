0395116216598a1ea222567fee78bbe0a1961515
Tanmay Vaij <tanmayvaij22@gmail.com>
2023-02-27 22:34:54 +0530

    added loaders in auth

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import AuthLoader from "../components/AuthLoader"
 import { GlobalStates } from "../context"
 
 export default function Signup() {
@@ -9,25 +10,29 @@ export default function Signup() {
         name: "", email: "", number: "", password: "", cpassword: ""
     })
 
+    const [ loader, setLoader ] = useState("hidden")
+
     const { name, email, number, password, cpassword } = state
 
     const signUp = async () => {
 
-        if ( name == "" || email == "" || number == "" || password == "" || cpassword == "" ) 
+        setLoader("flex")
+
+        if (name == "" || email == "" || number == "" || password == "" || cpassword == "")
             return alert("Some fields are missing")
-        
-        if ( password != cpassword ) return alert("Passwords are not matching")
-        
+
+        if (password != cpassword) return alert("Passwords are not matching")
+
         const res = await fetch(`${apiUrl}/api/auth/signup`, {
             method: "POST",
             headers: {
                 "Content-type": "application/json"
             },
-            body: JSON.stringify({ 
+            body: JSON.stringify({
                 name: name.trimEnd(),
-                email: email.trimEnd(), 
-                number: number.trimEnd(), 
-                password: password.trimEnd() 
+                email: email.trimEnd(),
+                number: number.trimEnd(),
+                password: password.trimEnd()
             })
         })
 
@@ -45,6 +50,11 @@ export default function Signup() {
     return (
         <div id="signup" className="pt-20 bg-gray-900 flex items-center justify-center flex-col min-h-screen">
 
+            <div className={`${loader} absolute top-40 items-center justify-center text-white`}>
+                <AuthLoader />
+                <span className="text-pink-500">Signing Up</span>
+            </div>
+
             <label className=" text-white block mb-2 text-sm font-medium dark:text-white">Your Name</label>
             <div className="relative mb-6">
                 <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
@@ -53,10 +63,10 @@ export default function Signup() {
                 <input
                     name="name"
                     value={state.name}
-                    onChange={ (e) => setState({ ...state, [e.target.name]: e.target.value }) }
-                    type="text" 
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-                    placeholder="Enter your name" 
+                    onChange={(e) => setState({ ...state, [e.target.name]: e.target.value })}
+                    type="text"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Enter your name"
                 />
             </div>
 
@@ -65,12 +75,12 @@ export default function Signup() {
                 <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                     <i className="fa-solid fa-envelope text-gray-500"></i>
                 </div>
-                <input 
+                <input
                     name="email"
                     value={state.email}
-                    onChange={ (e) => setState({ ...state, [e.target.name]: e.target.value }) }
-                    type="email" 
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                    onChange={(e) => setState({ ...state, [e.target.name]: e.target.value })}
+                    type="email"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Enter email address"
                 />
             </div>
@@ -80,13 +90,13 @@ export default function Signup() {
                 <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                     <i className="fa-solid fa-phone text-gray-500"></i>
                 </div>
-                <input 
+                <input
                     name="number"
                     value={state.number}
-                    onChange={ (e) => setState({ ...state, [e.target.name]: e.target.value }) }
-                    type="tel" 
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-                    placeholder="Enter phone number" 
+                    onChange={(e) => setState({ ...state, [e.target.name]: e.target.value })}
+                    type="tel"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Enter phone number"
                 />
             </div>
 
@@ -95,13 +105,13 @@ export default function Signup() {
                 <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                     <i className="fa-solid fa-lock text-gray-500"></i>
                 </div>
-                <input 
+                <input
                     name="password"
                     value={state.password}
-                    onChange={ (e) => setState({ ...state, [e.target.name]: e.target.value }) }                
-                    type="password" 
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-                    placeholder="Set Your password" 
+                    onChange={(e) => setState({ ...state, [e.target.name]: e.target.value })}
+                    type="password"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Set Your password"
                 />
             </div>
 
@@ -110,19 +120,19 @@ export default function Signup() {
                 <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                     <i className="fa-solid fa-lock text-gray-500"></i>
                 </div>
-                <input 
+                <input
                     name="cpassword"
                     value={state.cpassword}
-                    onChange={ (e) => setState({ ...state, [e.target.name]: e.target.value }) }
-                    type="password" 
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-                    placeholder="Confirm your password" 
+                    onChange={(e) => setState({ ...state, [e.target.name]: e.target.value })}
+                    type="password"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Confirm your password"
                 />
             </div>
 
-            <button 
+            <button
                 onClick={signUp}
-                type="button" 
+                type="button"
                 className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
             >
                 Sign Up
